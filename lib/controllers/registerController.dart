3be8635d6089dc0d43d5ba6e1babe96e54ccf856
Carlos Joczan Hernandez Morquecho <upper21m@gmail.com>
2025-06-18 09:38:32 -0600

    V1.0
    First version

--- lib/controllers/registerController.dart
+++ lib/controllers/registerController.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+/*Controller for register*/
 class RegisterController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
